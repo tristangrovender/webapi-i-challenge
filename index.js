15e6cca9818ddb8c5bc3a0a59198aa78ef49d217
Tristan Grovender <therealtristangrovender@gmail.com>
2019-04-22 15:33:44 -0500

    basic boilerplate completed, starting endpoints

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json()); // *** add this for POST
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5k ***\n');
+    
+});
